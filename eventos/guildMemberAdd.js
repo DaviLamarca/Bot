bf5853bee9e725bf163d7bc11e3eb81c49690013
DaviLamarca <davidiscordev@gmail.com>
2025-06-28 05:51:03 -0300

    atualização aviso oi v3

--- eventos/guildMemberAdd.js
+++ eventos/guildMemberAdd.js
@@ -6,7 +6,7 @@ module.exports = {
         let canal = await bot.channels.fetch("1386566946623062057")
         if (canal) {
             await canal.send(`Bem-vindo, <@${member.id}>!
-               Sou a HAL-9, IA da ORBITA-9. Regras em #regras. Qualquer coisa, estou por aqui! (Não sou uma IA de verdade.)`);
+Sou a HAL-9, IA da ORBITA-9. Regras em #regras. Qualquer coisa, estou por aqui! (Não sou uma IA de verdade.)`);
         } else {
             console.log("Não deu para dizer oi");
 
