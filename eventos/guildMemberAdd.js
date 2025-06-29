55bb69aaaaaefd6995dcf67bc18877076a6509a6
DaviLamarca <davidiscordev@gmail.com>
2025-06-29 10:30:11 -0300

    atualização

--- eventos/guildMemberAdd.js
+++ eventos/guildMemberAdd.js
@@ -2,16 +2,19 @@ module.exports = {
     nome: "guildMemberAdd",
     async executar(member, bot) {
         try {
-            const canalId = bot.adicionar.get(member.guild.id);
 
-            if (!canalId) return;
+            const canal = await bot.channels.fetch("1386566946623062057");
 
-            const canal = await bot.channels.fetch(canalId);
+            if (!canal) {
+                return
+            }
+
+            let cargo = await member.guild.roles.fetch("1388872136466370660")
 
             if (member.user.bot) {
                 await canal.send("🤖 Um novo bot entrou no servidor!");
             } else {
-                await canal.send(`👋 Olá, <@${member.id}>! Seja bem-vindo(a) ao servidor!`);
+                await canal.send(`👋 Olá, <@${member.id}>! Seja bem-vindo(a) ao servidor! Sou o bot oficial do servidor! @${cargo}`);
             }
         } catch (erro) {
             console.error("Erro ao enviar mensagem de boas-vindas:", erro);
