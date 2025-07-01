9ecbc4b262aad39969a45951c36455c2c98733c9
DaviLamarca <davidiscorddev@gmail.com>
2025-07-01 17:36:42 -0300

    atualização

--- eventos/guildMemberAdd.js
+++ eventos/guildMemberAdd.js
@@ -13,7 +13,7 @@ module.exports = {
             const cargo = await member.guild.roles.fetch("1388872136466370660")
 
             await canal.send(`👋 Olá, <@${member.id}>! Seja bem-vindo(a) ao servidor! Sou o bot oficial do servidor! ${cargo}`);
-            await canalAdm.send(`Novo usuário entrou ${member.id}`)
+            await canalAdm.send(`Novo usuário entrou. Informações sobre: ${member.displayName}, ${member.joinedAt}`)
         } catch (erro) {
             await canalErro.send("Erro em mandar mensagem:" + erro)
         }
