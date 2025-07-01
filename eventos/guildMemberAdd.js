97f7c42eb36db385b3cfa3e1d98fb3b1ad061d73
DaviLamarca <davidiscorddev@gmail.com>
2025-07-01 19:55:09 -0300

    atualização

--- eventos/guildMemberAdd.js
+++ eventos/guildMemberAdd.js
@@ -1,10 +1,10 @@
 module.exports = {
     nome: "guildMemberAdd",
     async executar(member, bot) {
-        try {
+        const canal = await bot.channels.fetch("1386566946623062057");
+        const canalAdm = await bot.channels.fetch("1388005828736319651")
 
-            const canal = await bot.channels.fetch("1386566946623062057");
-            const canalAdm = await bot.channels.fetch("1388005828736319651")
+        try {
 
             if (!canal) {
                 return
@@ -15,7 +15,7 @@ module.exports = {
             await canal.send(`👋 Olá, <@${member.id}>! Seja bem-vindo(a) ao servidor! Sou o bot oficial do servidor! ${cargo}`);
             await canalAdm.send(`Novo usuário entrou. Informações sobre: ${member.displayName}, ${member.joinedAt.toLocaleString(pt-BR)}`)
         } catch (erro) {
-            await canalErro.send("Erro em mandar mensagem:" + erro)
+            await canalAdm.send("Erro em mandar mensagem:" + erro)
         }
     }
 };
