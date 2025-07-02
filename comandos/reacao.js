9a48e9799305818dcad8f24fbab506cf11c8f12a
DaviLamarca <davidiscorddev@gmail.com>
2025-07-02 20:09:28 -0300

    atualizar

--- comandos/reacao.js
+++ comandos/reacao.js
@@ -1,4 +1,4 @@
-const { ActionRowBuilder, ButtonBuilder, ButtonStyle } = require('discord.js');
+const { ActionRowBuilder, ButtonBuilder, ButtonStyle, EmbedBuilder } = require('discord.js');
 
 module.exports = {
     nome: 'reacao',
@@ -31,8 +31,14 @@ module.exports = {
 
         const linha = new ActionRowBuilder().addComponents(botao1, botao2, botao3, botao4);
 
+        const embed = new EmbedBuilder()
+            .setColor(0x0099FF)
+            .setTitle('Escolha uma cor para receber um cargo')
+            .setDescription(
+                'Clique no botão para receber um dos cargos de cor disponíveis. Ao escolher uma cor, você receberá o cargo correspondente e, automaticamente, perderá o acesso a este canal. Isso significa que, após a escolha, você não poderá mais visualizar nem interagir neste canal específico, pois ele será ocultado para você. Pense bem antes de selecionar, pois a alteração é definitiva até que um administrador ou moderador faça a remoção do cargo para restaurar seu acesso. Caso tenha dúvidas, entre em contato com a equipe do servidor para obter ajuda.'
+            );
         await mensagem.reply({
-            content: 'Clique no botão para receber um dos cargos de cor disponíveis. Ao escolher uma cor, você receberá o cargo correspondente e, automaticamente, perderá o acesso a este canal. Isso significa que, após a escolha, você não poderá mais visualizar nem interagir neste canal específico, pois ele será ocultado para você. Pense bem antes de selecionar, pois a alteração é definitiva até que um administrador ou moderador faça a remoção do cargo para restaurar seu acesso. Caso tenha dúvidas, entre em contato com a equipe do servidor para obter ajuda.',
+            content: [embed],
             components: [linha]
         });
     }
