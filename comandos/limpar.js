f1dfb0f4895114749e766b8aadae8d245e69fbb3
DaviLamarca <davidiscorddev@gmail.com>
2025-07-08 20:06:40 -0300

    atualização

--- comandos/limpar.js
+++ comandos/limpar.js
@@ -1,10 +1,16 @@
 module.exports = {
     nome: 'limpar',
     async executar(mensagem, bot) {
+
+        let bodycomando = mensagem.content.trim();
+        let semCmd = bodycomando.split(" ");
+        let args = semCmd.slice(1);
+
         if (!mensagem.member.permissions.has("Administrator")) {
             return mensagem.reply("Você precisa ser administrador para usar este comando.");
         }
-        const listaMensagens = await mensagem.channel.messages.fetch({ limit: 100 })
+        const listaMensagens = await mensagem.channel.messages.fetch({ limit: Number(args[1]) })
+
         console.log(listaMensagens);
         await mensagem.channel.bulkDelete(listaMensagens)
         let msg = await mensagem.channel.send("Mensagens apagadas com sucesso!")
